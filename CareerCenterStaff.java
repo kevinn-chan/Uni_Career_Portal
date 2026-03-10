80838b47b2337a68b90852d5a5b33073d7a19e1a
Kevin Chan <chankangle.kevin@gmail.com>
2026-03-10 11:18:28 +0800

    Update CareerCenterStaff.java

--- CareerCenterStaff.java
+++ CareerCenterStaff.java
@@ -1,4 +1,4 @@
-package Internship_project;
+package internship_project;
 
 public class CareerCenterStaff extends User{
 	
@@ -12,15 +12,18 @@ public class CareerCenterStaff extends User{
 		return staffdepartment;}
 	
 	public void showMenu() {
-		System.out.println("Career Center Staff Menu: ");
-		System.out.println("-".repeat(50));
-		System.out.println("Hello" + super.getName() + "," + getStaffdepartment() + "!");
-		System.out.println("-".repeat(50));
+		System.out.println("\nCareer Center Staff Menu:"); 
+		System.out.println("-".repeat(40));
+		System.out.println("Hello " + super.getName() + " (" + getStaffdepartment() + ")!");
+		System.out.println("-".repeat(40));
 		System.out.println("What would you like to do?");
 		System.out.println("1. Manage Company Representatives Accounts");
 		System.out.println("2. Manage Internship Opportunities");
 		System.out.println("3. Manage Withdrawal Requests");
 		System.out.println("4. Generate Comprehensive Reports");
-		System.out.println("5. Change Password");
-		System.out.println("6. Logout");}
+		System.out.println("5. Set View Filters");
+		System.out.println("6. Change Password");
+		System.out.println("7. Logout");
+		System.out.println("-".repeat(40));
+	}
 }
